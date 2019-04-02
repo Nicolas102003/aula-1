ecfc31cd37f991ffd76db3d97517e537d95d153c
nicolas102003 <nicolas2003keko@gmail.com>
2019-04-01 21:21:23 -0300

    aula

---
+++ js/funcoes.js
@@ -0,0 +1,14 @@
+function trocaFonteMaior() {
+	document.getElementById("demo").style.fontSize="35px";
+}
+
+function trocaFonteMenor() {
+	document.getElementById("demo").style.fontSize="16px";
+	
+}
+function apagarElemento() {
+	document.getElementById("demo").style.display="block";
+}
+function naoApagarElemento() {
+	document.getElementById("demo").style.display="none";
+}
